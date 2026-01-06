5ca9399b02e1d77c1f4aaafe78d4c96736bee927
BQ06 <109698071+BQ06@users.noreply.github.com>
2026-01-06 15:36:45 +0000

    Util update pare_amount made

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import datetime
+from calendar import monthrange
+from decimal import Decimal, ROUND_HALF_UP
 
 # https://docs.python.org/3/library/datetime.html#aware-and-naive-objects used for date parsing understanding 
 
@@ -11,4 +13,25 @@ def parse_month(s: str):
     """Parse a month string in YYYY-MM format."""
     return datetime.datetime.strptime(s, '%Y-%m').date()
 
-def parse_
+def parse_amount(s: str):  # https://stackoverflow.com/questions/379906/how-do-i-parse-a-string-to-a-float-or-int
+    """Parse a amount str to an int.  12.50 == 1250 pennies"""
+    try:
+        return int(round(float(s) * 100))
+    except (ValueError, TypeError):
+        return print("Please enter a Valid amount")
+                        
+
+
+def format_money(pennies: int):
+    pounds = Decimal(pennies) / Decimal(100)
+    return f"£{pounds:.2f}"
+
+
+
+
+def validate_type(t: str):
+  return None
+
+def date_in_range(d: date, start: date, end: date):
+
+    return None
